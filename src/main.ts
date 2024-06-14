223768b9079e2a9c5c85ab7d336cccd6800baba4
JARUSS <tchaklidjirustico@gmail.com>
2024-06-14 15:59:08 +0000

    first commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   app.setGlobalPrefix("api/coffee");
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
