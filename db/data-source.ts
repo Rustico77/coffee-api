a2a5d2a30a17d0a384375d3405ba761103c622a5
unknown <igorzoulender@gmail.com>
2024-06-15 17:28:31 +0000

    Adding DB_URL into the data source

--- db/data-source.ts
+++ db/data-source.ts
@@ -4,11 +4,12 @@ import { DataSource, DataSourceOptions } from "typeorm";
 config()
 export const dataSourceOptions:DataSourceOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    // host: process.env.DB_HOST,
+    // port: Number(process.env.DB_PORT),
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DATABASE,
+    url: process.env.DB_URL,
     entities: ['dist/**/*.entity{.ts,.js}'],
     migrations: ['dist/db/migrations/*{.ts,.js}'],
     logging: false,
